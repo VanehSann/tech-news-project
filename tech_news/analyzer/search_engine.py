1a53eb01ef37cad126531915484ea017e2ab8cf9
VanehSann <vanessacbsantos22@gmail.com>
2023-01-24 02:38:39 -0300

    feat: implement 9 functions

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,43 @@
+from datetime import datetime
+
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, '$options': 'i'}})
+    result = []
+    for item in news:
+        result.append((item["title"], item["url"]))
+    return result
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        news = search_news({"timestamp": {"$regex": date}})
+        result = []
+        for item in news:
+            result.append((item["title"], item["url"]))
+        return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    news = search_news({"tags": {"$regex": tag, '$options': 'i'}})
+    result = []
+    for item in news:
+        result.append((item["title"], item["url"]))
+    return result
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"category": {"$regex": category, '$options': 'i'}})
+    result = []
+    for item in news:
+        result.append((item["title"], item["url"]))
+    return result
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,79 @@
+import time
+
+import requests
+from parsel import Selector
+
+from tech_news.database import create_news
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"})
+        time.sleep(1)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    urls = selector.css("a.cs-overlay-link::attr(href)").getall()
+    return urls
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("a.next::attr(href)").get()
+    return url
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("div.entry-header-inner h1.entry-title::text").get()
+    title = "".join(title).strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    comments_count = 0
+    summary = selector.css(
+        "div.entry-content > p:first-of-type *::text"
+        ).getall()
+    summary = "".join(summary).strip()
+    tags = selector.css("section.post-tags a::text").getall()
+    category = selector.css("a.category-style span.label::text").get()
+    newsList = {
+            "url": url,
+            "title": title,
+            "writer": writer,
+            "summary": summary,
+            "comments_count": comments_count,
+            "timestamp": timestamp,
+            "tags": tags,
+            "category": category,
+
+        }
+    return newsList
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    html_content = fetch("https://blog.betrybe.com/")
+    urls = scrape_updates(html_content)
+    while len(urls) < amount:
+        url_next_page = scrape_next_page_link(html_content)
+        html_content = fetch(url_next_page)
+        urls.extend(scrape_updates(html_content))
+    newsList = []
+    for item in range(amount):
+        html_content = fetch(urls[item])
+        newsList.append(scrape_news(html_content))
+    create_news(newsList)
+    return newsList
